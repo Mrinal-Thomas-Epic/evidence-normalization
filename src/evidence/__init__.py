85de9cd72da200e129967fd9ddc2c7160c03fa55
Mrinal-Thomas-Epic <126519671+Mrinal-Thomas-Epic@users.noreply.github.com>
2025-04-03 15:31:12 -0500

    Update src/evidence/__init__.py
    Co-authored-by: Kori Kuzma <korikuzma@gmail.com>

--- src/evidence/__init__.py
+++ src/evidence/__init__.py
@@ -5,5 +5,5 @@ from pathlib import Path
 
 APP_ROOT = Path(__file__).resolve().parents[0]
 DATA_DIR_PATH = environ.get("DATA_DIR_PATH", APP_ROOT / "data")
-if type(DATA_DIR_PATH) == str:
+if isinstance(DATA_DIR_PATH, str):
     DATA_DIR_PATH = Path(DATA_DIR_PATH)
